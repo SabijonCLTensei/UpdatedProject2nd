e0ea14912fce31887d2fbd80b08d35fe8fe0c960
SabijonCLTensei <sabijon.christian77@gmail.com>
2026-04-07 18:25:47 +0800

    Add files via upload

---
+++ Dump20260407 (1)/note_taking_app_db_category.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: note_taking_app_db
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (53,'App Ideas'),(56,'Art Sketches'),(20,'Book Summaries'),(52,'Bucket List'),(50,'Budgeting'),(59,'Car Logs'),(35,'Career Goals'),(62,'Client Feedback'),(19,'Coding Snippets'),(28,'Daily Habits'),(42,'Dream Log'),(27,'Event Planning'),(18,'Financial Records'),(15,'Fitness & Health'),(44,'Game Walkthroughs'),(32,'Gardening Tips'),(1,'General'),(34,'Gift Ideas'),(58,'Holiday Prep'),(33,'Home Maintenance'),(14,'Ideas & Brainstorming'),(61,'Interior Design'),(45,'Investment Research'),(31,'Language Learning'),(41,'Legal Documents'),(40,'Marketing Strategies'),(54,'Meditation & Mindfulness'),(22,'Meeting Minutes'),(36,'Mental Wellness'),(46,'Motivational Quotes'),(21,'Movies to Watch'),(29,'Music Inspiration'),(39,'Networking Contacts'),(3,'Personal'),(24,'Personal Journal'),(38,'Pet Care'),(37,'Photography Portfolio'),(47,'Podcasts to Listen'),(48,'Product Reviews'),(23,'Project Alpha'),(17,'Recipes'),(55,'Scientific Research'),(26,'Shopping List'),(25,'Side Hustles'),(49,'Social Media Content'),(4,'Study'),(60,'Subscription Tracking'),(30,'Tech News'),(16,'Travel Plans'),(63,'Uncategorized Chaos'),(57,'Volunteering'),(51,'Wishlist'),(2,'Work'),(43,'Writing Prompts');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-07 18:24:37
